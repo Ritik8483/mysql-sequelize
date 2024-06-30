227e73cfb649caf310cbbdb1d7978a3e27076b6d
Ritik Chauhan <rrajput6863@gmail.com>
2024-06-30 10:08:02 +0530

    (feat): complete crud api

--- index.js
+++ index.js
@@ -24,23 +24,25 @@
 //   console.log(`App listening on port ${port}`);
 // });
 
-
-
-
 //CONTROLLER
 const express = require("express");
 const app = express();
 const port = 8080;
 const cors = require("cors");
 const userAddUserCtrl = require("./controller/userController");
-const { addUser } = userAddUserCtrl;
+const { addUser, getUsers, getSingleUser, postUsers, deleteUser,updateUser } =
+  userAddUserCtrl;
 
 app.use(cors()); //cors policy for frontend
 app.use(express.json());
 
 //API ROUTE
-app.get("/get-user", addUser);    //dont call addUser it's a variable ; 
-app.post("/add-user", addUser);
+// app.get("/get-user", addUser);    //dont call addUser it's a variable ;
+app.get("/get-users", getUsers);
+app.get("/single-user/:id", getSingleUser);
+app.delete("/delete-user/:id", deleteUser);
+app.patch("/update-user/:id", updateUser);
+app.post("/add-user", postUsers);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
